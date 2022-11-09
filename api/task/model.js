31b36bbfb142e444c1de8edf01586a9056efff9b
Conor McKenna <Conorbmckenna@gmail.com>
2022-11-09 08:21:11 -0700

    Module Project Completed

--- api/task/model.js
+++ api/task/model.js
@@ -7,29 +7,42 @@ module.exports = {
 };
 
 function get(id) {
-    if(id){
-        return db("tasks").where("task_id", id).first().then((task)=>{
-            task.task_completed = task.task_completed ? true : false;
-            return task;
-        })
-    } else {
-        return db("tasks").then((tasks) => {
-            return tasks.map((item) => {
-              item.task_completed = item.task_completed ? true : false;
-              return item;
-            });
-          });
-    }
+  if (id) {
+    return db("tasks")
+      .where("task_id", id)
+      .first()
+      .then((task) => {
+        task.task_completed = task.task_completed ? true : false;
+        return task;
+      });
+  } else {
+    return db("tasks")
+      .join("projects", "projects.project_id", "tasks.project_id")
+      .select(
+        "tasks.task_id",
+        "tasks.task_description",
+        "tasks.task_notes",
+        "tasks.task_completed",
+        "projects.project_name",
+        "projects.project_description"
+      )
+      .then((tasks) => {
+        return tasks.map((item) => {
+          item.task_completed = item.task_completed ? true : false;
+          return item;
+        });
+      });
+  }
 }
 
 function insert(task) {
   return db("tasks")
     .insert(task)
     .then(([id]) => {
-        console.log("================================================================", id)
-        return get(id)
+      console.log(
+        "================================================================",
+        id
+      );
+      return get(id);
     });
-
 }
-
-
